7bd329f4a65b3812b7f9fe524c9f9b81e8ecd627
Mauro-Roncata <mauroroncata@gmail.com>
2026-03-16 15:11:41 -0300

    feat: creates entity Chamado

---
+++ src/main/java/br/com/edusupport/edusupport/entities/Chamado.java
@@ -0,0 +1,67 @@
+package br.com.edusupport.edusupport.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table (name = "tb_chamado")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Chamado {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String titulo;
+    @NotBlank
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String descricao;
+    private LocalDateTime dataAbertura;
+    private LocalDateTime dataFechamento;
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private StatusChamado status;
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private Prioridade prioridade;
+    @NotNull
+    private Categoria categoria;
+
+    public Chamado(String titulo, String descricao, Prioridade prioridade, Categoria categoria) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.prioridade = prioridade;
+        this.categoria = categoria;
+        this.status = StatusChamado.ABERTO;
+    }
+
+    @PrePersist
+    public void prePersist () {
+        this.dataAbertura = LocalDateTime.now();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Chamado chamado = (Chamado) o;
+        return Objects.equals(id, chamado.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
