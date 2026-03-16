04adeae4e9ad63bff690a0fe847b45378f5da67f
Mauro-Roncata <mauroroncata@gmail.com>
2026-03-16 10:35:18 -0300

    chore: initial commit with spring boot setup

---
+++ src/main/java/br/com/edusupport/edusupport/EdusupportApplication.java
@@ -0,0 +1,13 @@
+package br.com.edusupport.edusupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdusupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdusupportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/edusupport/edusupport/EdusupportApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.edusupport.edusupport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdusupportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
