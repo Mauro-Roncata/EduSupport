b8a40695eff01b3f3183b6dee55a2c406e9bcfb3
Mauro-Roncata <mauroroncata@gmail.com>
2026-03-25 15:05:28 -0300

    feat: implements records for dto

---
+++ src/main/java/br/com/edusupport/edusupport/dtos/ChamadoResponseDTO.java
@@ -0,0 +1,18 @@
+package br.com.edusupport.edusupport.dtos;
+
+import br.com.edusupport.edusupport.enums.Categoria;
+import br.com.edusupport.edusupport.enums.Prioridade;
+import br.com.edusupport.edusupport.enums.StatusChamado;
+
+import java.time.LocalDateTime;
+
+public record ChamadoResponseDTO(
+        Long id,
+        String titulo,
+        String descricao,
+        StatusChamado status,
+        Categoria categoria,
+        Prioridade prioridade,
+        LocalDateTime dataAbertura
+) {
+}
