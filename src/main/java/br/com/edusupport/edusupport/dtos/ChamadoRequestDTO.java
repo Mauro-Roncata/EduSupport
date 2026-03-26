7132269fc375d2154553fb7cd4bd10d485dc3e45
Mauro-Roncata <mauroroncata@gmail.com>
2026-03-26 14:17:05 -0300

    refactor: validating input data

--- src/main/java/br/com/edusupport/edusupport/dtos/ChamadoRequestDTO.java
+++ src/main/java/br/com/edusupport/edusupport/dtos/ChamadoRequestDTO.java
@@ -2,11 +2,19 @@ package br.com.edusupport.edusupport.dtos;
 
 import br.com.edusupport.edusupport.enums.Categoria;
 import br.com.edusupport.edusupport.enums.Prioridade;
-
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 public record ChamadoRequestDTO(
+        @NotBlank(message = "O título do chamado não pode estar vazio.")
         String titulo,
+
+        @NotBlank(message = "A descrição do problema é obrigatória.")
         String descricao,
+
+        @NotNull(message = "A categoria deve ser informada.")
         Categoria categoria,
+
+        @NotNull(message = "A prioridade deve ser informada.")
         Prioridade prioridade
 ) {
 }
