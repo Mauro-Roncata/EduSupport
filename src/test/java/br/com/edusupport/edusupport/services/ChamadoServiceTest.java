feabe2f87c05279425dfdd1c50fcb9d66310c019
Mauro-Roncata <mauroroncata@gmail.com>
2026-04-07 10:15:31 -0300

    test: Implements unit tests to close ticket

--- src/test/java/br/com/edusupport/edusupport/services/ChamadoServiceTest.java
+++ src/test/java/br/com/edusupport/edusupport/services/ChamadoServiceTest.java
@@ -1,6 +1,8 @@
 package br.com.edusupport.edusupport.services;
 
 import br.com.edusupport.edusupport.entities.Chamado;
+import br.com.edusupport.edusupport.enums.Categoria;
+import br.com.edusupport.edusupport.enums.Prioridade;
 import br.com.edusupport.edusupport.enums.StatusChamado;
 import br.com.edusupport.edusupport.repositories.ChamadoRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -48,8 +50,39 @@ public class ChamadoServiceTest {
         // Garante que o duble do banco de dados não salve nada
     verify(chamadoRepository, never()).save(any(Chamado.class));
 
+    }
+
+    @Test
+    @DisplayName("Deve resolver um chamado com sucesso quando estiver em andamento")
+    void deveResolverChamadoComSucesso() {
+
+        Long idDoChamado = 1L;
+        Chamado chamadoEmAndamento = new Chamado();
+        chamadoEmAndamento.setId(idDoChamado);
+        chamadoEmAndamento.setStatus(StatusChamado.EM_ANDAMENTO);
+
+        // Preenchendo dados mínimos
+        chamadoEmAndamento.setTitulo("Roteador sem luz");
+        chamadoEmAndamento.setDescricao("Luzes apagadas");
+        chamadoEmAndamento.setCategoria(Categoria.REDE_INTERNET); // Usando um Enum real
+        chamadoEmAndamento.setPrioridade(Prioridade.ALTA);
+
+        // Quando buscar o ID 1, devolva o chamado que está em andamento
+        when(chamadoRepository.findById(idDoChamado)).thenReturn(Optional.of(chamadoEmAndamento));
+
+        // Ensinando o robô do Banco a salvar.
+        when(chamadoRepository.save(any(Chamado.class))).thenAnswer(invocacao -> invocacao.getArgument(0));
 
 
+        // Chamamando o método oficial. Ele deve processar tudo e nos devolver o DTO.
+        var chamadoResolvido = chamadoService.resolverChamado(idDoChamado);
 
+
+        assertNotNull(chamadoResolvido); // Garante que não voltou vazio
+        assertEquals(StatusChamado.RESOLVIDO, chamadoResolvido.status()); // Garante que o Java alterou o status
+        assertNotNull(chamadoResolvido.dataFechamento()); // Garante que o Java carimbou a data final
+
+        // Confere se o robô do banco foi acionado EXATAMENTE 1 vez para salvar
+        verify(chamadoRepository, times(1)).save(any(Chamado.class));
     }
 }
